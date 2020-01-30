fa0d2ac5f18d51061267b70ee60c761d91d2a8fd
btapiaf <57454001+btapiaf@users.noreply.github.com>
2020-01-29 19:26:21 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const app = express();
+const express = require('express'); //exportamos express
+const app = express(); // llamamos a la funcion express
 
-const hbs = require('hbs');
+const hbs = require('hbs'); //requerimos el modulo hms
 require('./hbs/helpers');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; //escucha un puerto que este libre o el puerto 3000
 
 app.use(express.static(__dirname + '/public'));
 
 // Express HBS engine
-hbs.registerPartials(__dirname + '/views/parciales');
+hbs.registerPartials(__dirname + '/views/parciales'); //creamos las vistas con hbs
 app.set('view engine', 'hbs');
 
-app.get('/', function(req, res) {
+app.get('/', function(req, res) { // creamos nuestra ruta con el metodo get que solo nos sirve para recibir datos
     res.render('home', {
-        nombre: "rodRigo tuFIño"
+        nombre: "BoRiS TaPia"
     });
 });
 
-app.get('/about', (req, res) => {
+app.get('/about', (req, res) => { //creamos una ruta llamada about con el metodo get 
     //res.send('Esta es mi primera web app');
     res.render('about');
 });
 
-app.listen(port, () => {
+app.listen(port, () => { //escuchamos el puerto que se encuentra
     console.log(`Escuchando peticiones en el puerto ${port}`);
 });
